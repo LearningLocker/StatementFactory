63ae28d1ac3f0a6216facaf1950332d95f831686
Ryan Smith <0ryansmith1994@gmail.com>
2014-12-30 09:53:25 +0000

    Avoids encoding in messages.

--- src/Errors/MissingProperties.php
+++ src/Errors/MissingProperties.php
@@ -2,8 +2,8 @@
 
 class MissingProperties extends Error {
   public function __construct(array $properties, $class) {
-    $properties_string = json_encode($properties);
-    $message = "Missing properties $properties_string from `$class`";
+    $properties_string = implode(', ', $properties);
+    $message = "Missing properties ($properties_string) from `$class`";
     parent::__construct($message);
   }
 }
--- src/Errors/UnknownProperties.php
+++ src/Errors/UnknownProperties.php
@@ -2,8 +2,8 @@
 
 class UnknownProperties extends Error {
   public function __construct(array $properties, $class) {
-    $properties_string = json_encode($properties);
-    $message = "Unknown properties $properties_string from `$class`";
+    $properties_string = implode(', ', $properties);
+    $message = "Unknown properties ($properties_string) from `$class`";
     parent::__construct($message);
   }
 }
