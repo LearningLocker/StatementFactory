4fc7d41bd8b2037285c67b2808e9b65477433e69
Ryan Smith <0ryansmith1994@gmail.com>
2015-02-06 13:57:55 +0000

    Fixes object as contextActivity.

--- src/Context.php
+++ src/Context.php
@@ -25,4 +25,11 @@ class ContextActivities extends Element {
 
 class Activities extends Collection {
   protected $member_type = 'Locker\XApi\Activity';
+
+  public function setValue($new_value) {
+    if (is_object($new_value)) {
+      $new_value = [$new_value];
+    }
+    return parent::setValue($new_value);
+  }
 }
