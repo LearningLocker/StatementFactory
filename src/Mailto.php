cd46e9f3a67e43afa8475a0b35dafbd979d5d5e1
James Mullaney <james@ht2.co.uk>
2016-02-22 09:10:26 +0000

    Add backtick to Mailto regex pattern
    Addresses issue #10

--- src/Mailto.php
+++ src/Mailto.php
@@ -1,5 +1,5 @@
 <?php namespace Locker\XApi;
 
 class Mailto extends RegexpAtom {
-  protected static $pattern = '/^mailto:[A-Z0-9.\'_%+-]+@[A-Z0-9.-]+\.[A-Z]{1,63}$/i';
+  protected static $pattern = '/^mailto:[A-Z0-9.`\'_%+-]+@[A-Z0-9.-]+\.[A-Z]{1,63}$/i';
 }
